02dd631447334d594b2f1d7ecf5dfa56728d23e4
Akintunde Toba <atoba@ITILP_L002.simplexsystems.com>
2019-09-07 13:17:42 +0100

    first commit

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIEnd2EndEncryption.Models;
+using WebAPIEnd2EndEncryption.SHA512EncryptionEngine;
+
+namespace WebAPIEnd2EndEncryption.Controllers
+{
+    public class PaymentController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetPayment()
+        {
+            return Request.CreateResponse(new
+            {
+                error = false,
+                message = "Payments"
+            });
+        }
+        [HttpPost]
+        public HttpResponseMessage PostPayment([FromBody]Payment payment)
+        {
+            try
+            {
+                if (payment == null) return Request.CreateResponse(new
+                {
+                    error = true,
+                    message = "Bad Request"
+                });
+
+                string rawValue = $"{payment.TotalAmount}{payment.Currency}{payment.TransReference}";
+                string computeHash = EncryptionHelper.EncryptSha512(rawValue);
+                if (payment.Hash.ToLower() != computeHash.ToLower()) return Request.CreateResponse(new
+                {
+                    error = true,
+                    message = "Invalid hash detected"
+                });
+
+                return Request.CreateResponse(new
+                {
+                    error = false,
+                    message = "Payment Processed successfully"
+                });
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(new
+                {
+                    error = true,
+                    message = "Error occured while processing your request"
+                });
+            }
+        }
+    }
+}
---
+++ SHA512EncryptionEngine/EncryptionHelper.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Security.Cryptography;
+
+namespace WebAPIEnd2EndEncryption.SHA512EncryptionEngine
+{
+    public static class EncryptionHelper
+    {
+        const string HASHSALT = "d0k8hRzrjRkWOVLd28fwpDZlrm1Vuv3eoyEhzJeBIVpXbInxkoSOr5qZWRFINjsl7SVyW1hdVZ5dbJKmOu9Bibcv8qFIxs5hvgKvTReEggMlmByVBMdzKvSuDwtwscG9mKuSt505ulVPD6GcbYDrcZ";
+        public static string EncryptSha512(string value)
+        {
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(HASHSALT);
+            HMACSHA512 hMACSHA = new HMACSHA512(keyByte);
+
+            byte[] messageBytes = encoding.GetBytes(value);
+            byte[] hashmessage = hMACSHA.ComputeHash(messageBytes);
+
+            return ByteToString(hashmessage);
+        }
+
+        private static string ByteToString(byte[] buff)
+        {
+            string sbinary = "";
+
+            for (int i = 0; i < buff.Length; i++)
+            {
+                sbinary += buff[i].ToString("X2"); // hex format
+            }
+            return (sbinary);
+        }
+    }
+}
